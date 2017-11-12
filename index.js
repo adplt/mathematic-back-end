ce497cf0421fb7c5687c47042862a8e08f6830a7
adplt <atriadplt@gmail.com>
2017-11-12 09:35:37 +0700

    restructure project

---
+++ index.js
@@ -0,0 +1,51 @@
+import app from './app';
+import debug from 'debug';
+import http from 'http';
+
+require('babel-register')();
+debug('mathematic-back-end:server');
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) return val;
+  if (port >= 0) return port;
+  return false;
+};
+
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+  case 'EACCES':
+    process.exit(1);
+    break;
+  case 'EADDRINUSE':
+    process.exit(1);
+    break;
+  default:
+    throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+};
+
+const server = http.createServer(app);
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
